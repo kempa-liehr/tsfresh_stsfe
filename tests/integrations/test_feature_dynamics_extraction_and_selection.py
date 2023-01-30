0898667aec77a7b4e75ec11ad85065b58cdc24fa
Scott-Simmons <ssim323@aucklanduni.ac.nz>
2023-01-31 09:10:28 +1300

    integration test for engineering timeseries and then extracting feature dynamics

--- tests/integrations/test_feature_dynamics_extraction_and_selection.py
+++ tests/integrations/test_feature_dynamics_extraction_and_selection.py
@@ -19,6 +19,8 @@ from tsfresh.feature_dynamics_extraction.feature_dynamics_utils import (
     diff_between_series
 )
 
+from typing import List, Dict
+
 
 
 # WARNING/NOTE:: This file is still in progress! Highly likely that some really big changes will be made to this whole file 
@@ -55,9 +57,7 @@ class FeatureDynamicsExtractionTestCase(TestCase):
         # extract_feature_dynamics()
 
 
-class EndToEndTestDataCase(TestCase):
-
-
+class DataForFeatureDynamicsIntegrationTests(TestCase):
     def column_params_picker(self, data_format):
         """
         Picks out the correct column params depending on 
@@ -67,13 +67,9 @@ class EndToEndTestDataCase(TestCase):
         if data_format not in ["wide", "long", "dict"]:
             raise ValueError
 
-        print("For now we havent implemented long and dict")
-        #TODO: Remove once implemented more stuff 
-        data_format = "wide"
-
         if data_format == "wide":
             return {
-                "column_sort":None,
+                "column_sort":"t",
                 "column_kind":None,
                 "column_id": "measurement_id",
                 "column_value":None
@@ -89,7 +85,7 @@ class EndToEndTestDataCase(TestCase):
 
         elif data_format == "dict":
             return {
-                "column_sort":"sort",
+                "column_sort":"t",
                 "column_kind":None,
                 "column_id": "measurement_id",
                 "column_value":"value"
@@ -105,18 +101,10 @@ class EndToEndTestDataCase(TestCase):
         elif feature_complexity == "not-minimal":
             # Get a reasonably sized subset of somewhat comeplex features
             non_simple_features_fc_parameters = {
-                "agg_autocorrelation": [{'f_agg':"mean",'maxlag':40}],
-                "augmented_dickey_fuller": [{'attr':"pvalue",'autolag':"BIC"}],
-                "mean_second_derivative_central": None,
-                "last_location_of_maximum": None,
                 "fft_coefficient": [{"coeff":1, "attr":"real"}],
-                "fft_aggregated": [{"aggtype":"variance"}],
-                "number_peaks": [{"n":3}],
                 "number_cwt_peaks": [{"n":3}],
-                "linear_trend": [{"attr":"slope"}],
                 "permutation_entropy":[{"tau":1, "dimension":2}], 
                 "quantile": [{'q':0.2}],
-                "benford_correlation": None
             }
             
             return non_simple_features_fc_parameters
@@ -386,21 +374,42 @@ class EndToEndTestDataCase(TestCase):
             "10",
         ]
 
-        ts = pd.DataFrame(
-            {
-                "t": np.repeat([1, 2, 3, 4, 5, 6], 10),
-                "y1": np.asarray(y1, dtype=float),
-                "y2": np.asarray(y2, dtype=float),
-                "y3": np.asarray(y3, dtype=float),
-                "measurement_id": np.asarray(measurement_id, dtype=int),
-            }
-        )
+        df = pd.DataFrame(
+                {
+                    "t": np.repeat([1, 2, 3, 4, 5, 6], 10),
+                    "y1": np.asarray(y1, dtype=float),
+                    "y2": np.asarray(y2, dtype=float),
+                    "y3": np.asarray(y3, dtype=float),
+                    "measurement_id": np.asarray(measurement_id, dtype=int),
+                }
+            )
 
-        if data_format != "wide":
-            print("Haven't yet got around to other formats")
+        if data_format == "wide":
 
-        if container_type == "dask":
-            ts = dd.from_pandas(ts, npartitions=3)
+            ts = df
+            if container_type == "dask":
+                ts = dd.from_pandas(ts, npartitions=3)
+        
+        elif data_format == "long":
+
+            ts = pd.melt(
+                df, id_vars = ["measurement_id", "t"], value_vars=["y1","y2","y3"], value_name="value",var_name="kind"
+            ).reset_index(drop=True)
+
+            if container_type == "dask":
+                ts = dd.from_pandas(ts, npartitions=3)
+
+
+        elif data_format == "dict":
+
+            ts = {kind : pd.DataFrame() for kind in ["y1","y2","y3"]}
+            for kind in ts.keys():
+                ts[kind] = df[["t"] + [kind] + ["measurement_id"]].rename(columns={kind:"value"}).reset_index(drop=True)
+
+            if container_type == "dask":
+                for kind in ts.keys():
+                    ts[kind] = dd.from_pandas(ts[kind], npartitions = 3)
+            
 
         response = (
             pd.DataFrame(
@@ -417,97 +426,181 @@ class EndToEndTestDataCase(TestCase):
 
 
 
-class SelectFeaturesAndExtractTestCase(TestCase):
-    """
-    Tests for extracting features, 
-    selecting relevant features, then
-    saving the feature names to a dictionary,
-    and then extracting more relevant feature 
-    dynamics.
-    """
-
-    def test_select_features_from_minimal_extraction_from_engineered_timeseries(self):
-
-        pass
-
-    def test_select_features_from_efficient_extraction_from_engineered_timeseries(self):
-        pass
-
-
-    # Pandas minimal (a,b,c 3 input formats)
-    # def pandas minimal input format a
-    # def pandas minimal input format b
-    # def pandas minimal input format c
-    # extract ---> select ---> save relevant --- extract on relevant
-
-    # Pandas efficient (a,b,c 3 input formats) 
-    # def pandas efficient input format a
-    # def pandas efficient input format b
-    # def pandas efficient input format c
-    # extract ---> select ---> save relevant --- extract on relevant
-
-    # Dask minimal (3 formats but as dask dds)
-    # def Dask minimal input format a
-    # def Dask minimal input format b
-    # def Dask minimal input format c
-    # extract ---> select ---> save relevant --- extract on relevant
-
-
-    # Dask efficient (3 formats but as dask dds)
-    # def Dask efficient input format a
-    # def Dask efficient input format b
-    # def Dask efficient input format c
-    # extract ---> select ---> save relevant --- extract on relevant
-
-
-
-class InterpretFeatureDynamicsTestCase(TestCase):
-    """
-    Tests for taking a very long feature dynamics names and 
-    generating a pdf which interprets what these feature names 
-    actually mean
-    """
-
-    def test_generate_pdf(self):
-        pass
-
-class EngineerMoreTsTestCase(TestCase):
+class EngineerMoreTsTestCase(DataForFeatureDynamicsIntegrationTests):
     """
     Tests for engineering more timeseries, 
     and then extracting feature dynamics based 
     on these.
     """
-    pass
 
     # Engineer on pandas (3 input formats, large and small dicts)
     # Engineer on pandas then convert to dask check it still works (3 formats, large and small dicts)
+
+    def check_correct_ts_are_engineered(self, expected_ts:List, timeseries_container, data_format:str, column_params_config:Dict):
+
+        if data_format == "wide":
+            self.assertEqual(set(expected_ts), set(timeseries_container.columns))
+        
+        elif data_format == "long":
+            long_columns = set(timeseries_container.columns)
+            self.assertTrue(column_params_config["column_kind"] in long_columns and column_params_config["column_value"] in long_columns)
+            long_columns.remove(column_params_config["column_kind"])
+            long_columns.remove(column_params_config["column_value"])
+            ts_kinds = set(timeseries_container[column_params_config["column_kind"]])
+            self.assertEqual(set(expected_ts), set.union(set(timeseries_container[list(long_columns)]), ts_kinds))
+            
+        elif data_format == "dict":
+            dict_columns = set()
+            for df in timeseries_container.values():
+                dict_columns.update(df.columns.tolist())
+
+            self.assertTrue(column_params_config["column_value"] in dict_columns)
+            dict_columns.remove(column_params_config["column_value"])
+
+            self.assertEqual(set(expected_ts), set.union(set(timeseries_container.keys()), dict_columns))
+        
+        else:
+            raise ValueError
     
-    def test_engineer_more_ts_and_then_extraction_on_pandas_minimal(self):
-        pass
+    def test_engineer_more_ts_and_then_extraction_on_pandas_wide(self):
+        
+        data_format = "wide"
 
-    def test_engineer_more_ts_and_then_extraction_on_pandas_efficient(self):
-        pass
+        ts, response = self.gen_example_timeseries_data_for_e2e_tests(container_type = "pandas", data_format = data_format)
+        column_params_config = self.column_params_picker(data_format=data_format)
+        
+        # Engineer some more timeseries from input timeseries [D_y1y2, D_y1y3, D_y2y3]
+        ts_with_extra_timeseries_between = diff_between_series(ts, column_id = column_params_config["column_id"], column_sort = column_params_config["column_sort"], column_value = column_params_config["column_value"], column_kind = column_params_config["column_kind"])
+        expected_ts = ["t", "measurement_id", "y1", "y2", "y3", "D_y1y2", "D_y1y3", "D_y2y3"]
+        self.check_correct_ts_are_engineered(expected_ts, ts_with_extra_timeseries_between, data_format, column_params_config)
+        
+        # add an even extra layer of ts differencing 
+        ts_with_extra_timeseries_between_and_within = diff_within_series(ts_with_extra_timeseries_between, column_id = column_params_config["column_id"], column_sort = column_params_config["column_sort"], column_value = column_params_config["column_value"], column_kind = column_params_config["column_kind"])
+        expected_ts += ["dt_y1", "dt_y2", "dt_y3", "dt_D_y1y2", "dt_D_y1y3", "dt_D_y2y3"]
+        self.check_correct_ts_are_engineered(expected_ts, ts_with_extra_timeseries_between_and_within, data_format, column_params_config)
+        
+        # Extract feature dynamics
+        fts_fcs = self.gen_feature_calculators_for_e2e_tests(feature_complexity = "not-minimal")
+        fd_fcs =  self.gen_feature_calculators_for_e2e_tests(feature_complexity = "not-minimal")
+        window_length_1 = 4
+        window_length_2 = 5
+        fts_fcs_with_window_lengths = {window_length_1:fts_fcs, window_length_2:fts_fcs}
+        fts_fds_with_window_lengths = {window_length_1:fd_fcs, window_length_2:fd_fcs}
+        X = extract_feature_dynamics(
+            timeseries_container = ts_with_extra_timeseries_between_and_within,
+            feature_timeseries_fc_parameters = fts_fcs_with_window_lengths,
+            feature_dynamics_fc_parameters = fts_fds_with_window_lengths,
+            column_id = column_params_config["column_id"],
+            column_sort = column_params_config["column_sort"],
+            column_kind = column_params_config["column_kind"],
+            column_value = column_params_config["column_value"] 
+        )
+        some_expected_feature_dynamics_names = (
+            'dt_y2||quantile||q_0.2@window_5__fft_coefficient__attr_"real"__coeff_1',
+            'y2||number_cwt_peaks||n_3@window_5__number_cwt_peaks__n_3',
+            'D_y2y3||permutation_entropy||dimension_2||tau_1@window_4__quantile__q_0.2',
+            'dt_D_y1y2||fft_coefficient||attr_"real"||coeff_1@window_4__number_cwt_peaks__n_3',
+        )
+        
+        self.assertTrue(set(some_expected_feature_dynamics_names).issubset(X.columns.tolist()))
+        self.assertIsInstance(X, pd.DataFrame)
+        self.assertTrue(len(X) == 10)
+        # We cant make strong claims about the number of features produced because some feature timeseries have NaNs and are dropped
 
-    def test_dynamics_extraction_on_pandas_minimal(self):
-        pass
+    def test_engineer_more_ts_and_then_extraction_on_pandas_long(self):
 
-    def test_dynamics_extraction_on_pandas_efficient(self):
-        pass
+        data_format = "long"
+        
+        ts, response = self.gen_example_timeseries_data_for_e2e_tests(container_type = "pandas", data_format = data_format)
+        column_params_config = self.column_params_picker(data_format=data_format)
+        
+        # a) Engineer some more timeseries from input timeseries [D_y1y2, D_y1y3, D_y2y3]
+        ts_with_extra_timeseries_between = diff_between_series(ts, column_id = column_params_config["column_id"], column_sort = column_params_config["column_sort"], column_value = column_params_config["column_value"], column_kind = column_params_config["column_kind"])
+        expected_ts = ["t", "measurement_id", "y1", "y2", "y3", "D_y1y2", "D_y1y3", "D_y2y3"]
+        self.check_correct_ts_are_engineered(expected_ts, ts_with_extra_timeseries_between, data_format, column_params_config)
+        
+        # add an even extra layer of ts differencing 
+        ts_with_extra_timeseries_between_and_within = diff_within_series(ts_with_extra_timeseries_between, column_id = column_params_config["column_id"], column_sort = column_params_config["column_sort"], column_value = column_params_config["column_value"], column_kind = column_params_config["column_kind"])
+        expected_ts += ["dt_y1", "dt_y2", "dt_y3", "dt_D_y1y2", "dt_D_y1y3", "dt_D_y2y3"]
+        self.check_correct_ts_are_engineered(expected_ts, ts_with_extra_timeseries_between_and_within, data_format, column_params_config)
+        
+        # Extract feature dynamics
+        fts_fcs = self.gen_feature_calculators_for_e2e_tests(feature_complexity = "not-minimal")
+        fd_fcs =  self.gen_feature_calculators_for_e2e_tests(feature_complexity = "not-minimal")
+        window_length_1 = 4
+        window_length_2 = 5
+        fts_fcs_with_window_lengths = {window_length_1:fts_fcs, window_length_2:fts_fcs}
+        fts_fds_with_window_lengths = {window_length_1:fd_fcs, window_length_2:fd_fcs}
+        X = extract_feature_dynamics(
+            timeseries_container = ts_with_extra_timeseries_between_and_within,
+            feature_timeseries_fc_parameters = fts_fcs_with_window_lengths,
+            feature_dynamics_fc_parameters = fts_fds_with_window_lengths,
+            column_id = column_params_config["column_id"],
+            column_sort = column_params_config["column_sort"],
+            column_kind = column_params_config["column_kind"],
+            column_value = column_params_config["column_value"] 
+        )
+        some_expected_feature_dynamics_names = (
+            'dt_y2||quantile||q_0.2@window_5__fft_coefficient__attr_"real"__coeff_1',
+            'y2||number_cwt_peaks||n_3@window_5__number_cwt_peaks__n_3',
+            'D_y2y3||permutation_entropy||dimension_2||tau_1@window_4__quantile__q_0.2',
+            'dt_D_y1y2||fft_coefficient||attr_"real"||coeff_1@window_4__number_cwt_peaks__n_3',
+        )
+        
+        self.assertTrue(set(some_expected_feature_dynamics_names).issubset(X.columns.tolist()))
+        self.assertIsInstance(X, pd.DataFrame)
+        self.assertTrue(len(X) == 10)
+        # We cant make strong claims about the number of features produced because some feature timeseries have NaNs and are dropped
 
-    def test_dynamics_extraction_on_dask_minimal(self):
-        pass
 
-    def test_making_many_combos_of_differences(self):
-        pass
-        # ts_with_extra_timeseries = diff_within_series()
-        # ts_with_extra_timeseries = diff_between_series()
+    def test_engineer_more_ts_and_then_extraction_on_pandas_dict(self):
 
-        # Apply the differences function repeatedly and test it works alright
+        data_format = "dict"
+        
+        ts, response = self.gen_example_timeseries_data_for_e2e_tests(container_type = "pandas", data_format = data_format)
+        column_params_config = self.column_params_picker(data_format=data_format)
+        # a) Engineer some more timeseries from input timeseries [D_y1y2, D_y1y3, D_y2y3]
+        ts_with_extra_timeseries_between = diff_between_series(ts, column_id = column_params_config["column_id"], column_sort = column_params_config["column_sort"], column_value = column_params_config["column_value"], column_kind = column_params_config["column_kind"])
+        expected_ts = ["t", "measurement_id", "y1", "y2", "y3", "D_y1y2", "D_y1y3", "D_y2y3"]
+        self.check_correct_ts_are_engineered(expected_ts, ts_with_extra_timeseries_between, data_format, column_params_config)
+        
+        # add an even extra layer of ts differencing 
+        ts_with_extra_timeseries_between_and_within = diff_within_series(ts_with_extra_timeseries_between, column_id = column_params_config["column_id"], column_sort = column_params_config["column_sort"], column_value = column_params_config["column_value"], column_kind = column_params_config["column_kind"])
+        expected_ts += ["dt_y1", "dt_y2", "dt_y3", "dt_D_y1y2", "dt_D_y1y3", "dt_D_y2y3"]
+        self.check_correct_ts_are_engineered(expected_ts, ts_with_extra_timeseries_between_and_within, data_format, column_params_config)
+        
+        # Extract feature dynamics
+        fts_fcs = self.gen_feature_calculators_for_e2e_tests(feature_complexity = "not-minimal")
+        fd_fcs =  self.gen_feature_calculators_for_e2e_tests(feature_complexity = "not-minimal")
+        window_length_1 = 4
+        window_length_2 = 5
+        fts_fcs_with_window_lengths = {window_length_1:fts_fcs, window_length_2:fts_fcs}
+        fts_fds_with_window_lengths = {window_length_1:fd_fcs, window_length_2:fd_fcs}
+        X = extract_feature_dynamics(
+            timeseries_container = ts_with_extra_timeseries_between_and_within,
+            feature_timeseries_fc_parameters = fts_fcs_with_window_lengths,
+            feature_dynamics_fc_parameters = fts_fds_with_window_lengths,
+            column_id = column_params_config["column_id"],
+            column_sort = column_params_config["column_sort"],
+            column_kind = column_params_config["column_kind"],
+            column_value = column_params_config["column_value"] 
+        )
+        some_expected_feature_dynamics_names = (
+            'dt_y2||quantile||q_0.2@window_5__fft_coefficient__attr_"real"__coeff_1',
+            'y2||number_cwt_peaks||n_3@window_5__number_cwt_peaks__n_3',
+            'D_y2y3||permutation_entropy||dimension_2||tau_1@window_4__quantile__q_0.2',
+            'dt_D_y1y2||fft_coefficient||attr_"real"||coeff_1@window_4__number_cwt_peaks__n_3',
+        )
+        
+        self.assertTrue(set(some_expected_feature_dynamics_names).issubset(X.columns.tolist()))
+        self.assertIsInstance(X, pd.DataFrame)
+        self.assertTrue(len(X) == 10)
+        # We cant make strong claims about the number of features produced because some feature timeseries have NaNs and are dropped
 
 
-class FullEndToEndFeatureDynamicsWorkflowTestCase(EndToEndTestDataCase):
+class FullEndToEndFeatureDynamicsWorkflowTestCase(DataForFeatureDynamicsIntegrationTests):
     """
-    Test for:
+    Test the integrations related to the following workflow:
 
     a) Engineer more timeseries then
     b) Extract features then
